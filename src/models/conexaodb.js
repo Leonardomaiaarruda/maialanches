5c552937f209a81faaf61e3c4f04599da55febc2
Leonardo Maia <76956348+Leonardomaiaarruda@users.noreply.github.com>
2024-04-28 21:44:57 -0300

    Update conexaodb.js

--- src/models/conexaodb.js
+++ src/models/conexaodb.js
@@ -1,10 +1,1 @@
-import { Sequelize } from "sequelize";
 
-// Criar uma nova instância do Sequelize
-const sequelize = new Sequelize('dog', 'root', '0000', {
-  host: 'localhost', // Host do banco de
-  dialect: 'mysql'   // Tipo do banco de  dadosdados 
-});
-
-
-export default sequelize;
